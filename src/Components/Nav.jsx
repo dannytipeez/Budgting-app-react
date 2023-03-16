7e4ea8ea35df565c67a8a09062ca10e07e49037f
Chief <freakoutbond2@gmail.com>
2023-03-16 17:49:37 +0300

    added nav and icons pack

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Form, NavLink } from 'react-router-dom'
+
+//assets
+import logomark from '../assets/logomark.svg';
+
+//delete icon
+import { TrashIcon } from '@heroicons/react/24/solid'
 
 function Nav({ user }) {
     return (
         <nav>
-            <h3>{user}</h3>
+            <NavLink to="/" aria-label="Go to Home">
+                <img src={logomark} alt="logo" height={30} />
+                <span>HomeBudget</span>
+            </NavLink>
+            {
+                user && (
+                    <Form method='post' action='/logout' onSubmit={(event) => {
+                        if (!confirm("Delete data and user!")) {
+                            event.preventDefault();
+                        }
+                    }}>
+                        <button type="submit" className='btn btn--warning'>
+                            <span>Delete User</span>
+                            <TrashIcon width={20} />
+                            
+                        </button>
+                    </Form>
+                )
+            }
         </nav>
     )
 }
